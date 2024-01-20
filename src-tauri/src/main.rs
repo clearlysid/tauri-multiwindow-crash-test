594730318af32c98162774146bbd3cbc4b88c4e0
Siddharth <siddharth99c@gmail.com>
2024-01-20 22:33:36 +0530

    removes fluff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,12 +14,12 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             // Create a second window at runtime
-            tauri::WindowBuilder::new(app, "second", tauri::WindowUrl::App("/second".into()))
-                .inner_size(540.0, 480.0)
-                .title("tauri-frontend-bug Window 2")
-                .center()
-                .build()
-                .expect("couldn't create a second window");
+            // tauri::WindowBuilder::new(app, "second", tauri::WindowUrl::App("/second".into()))
+            //     .inner_size(540.0, 480.0)
+            //     .title("tauri-frontend-bug Window 2")
+            //     .center()
+            //     .build()
+            //     .expect("couldn't create a second window");
 
             Ok(())
         })
