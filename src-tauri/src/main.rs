de651ecd39b0e559083346620ac01ea65e2658aa
Siddharth <siddharth99c@gmail.com>
2024-01-21 10:27:38 +0530

    removes more fluff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Window;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             // Create a second window at runtime
             // tauri::WindowBuilder::new(app, "second", tauri::WindowUrl::App("/second".into()))
